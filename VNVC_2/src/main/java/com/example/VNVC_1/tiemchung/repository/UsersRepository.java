44c013813bf2a8cf45a623332bed1c693fb4b856
hienn12454 <hien12454@gmail.com>
2025-02-11 13:17:10 +0700

    Ver2

---
+++ VNVC_2/src/main/java/com/example/VNVC_1/tiemchung/repository/UsersRepository.java
@@ -0,0 +1,24 @@
+package com.example.VNVC_1.tiemchung.repository;
+
+import com.example.VNVC_1.tiemchung.model.RoleUsers;
+import com.example.VNVC_1.tiemchung.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+
+    Optional<Users> findByUsername(String username);
+
+    // Tìm danh sách trẻ em theo ID cha mẹ
+    List<Users> findByParentUserId(Long parentId);
+
+    // Xóa lịch booking theo ID người dùng
+    void deleteBookingByUserId(Long userId);
+
+    List<Users> findByRole(RoleUsers role);
+
+}
