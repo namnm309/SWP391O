36567f2ce5dc12d5b8334e778092a3e2e0054e8a
NamNguyen <namnm309@gmail.com>
2025-02-13 11:21:08 +0700

    Revert "ver2-05-02-2025"

--- VNVC_1/src/main/java/com/example/VNVC_1/tiemchung/repository/UsersRepository.java
+++ VNVC_1/src/main/java/com/example/VNVC_1/tiemchung/repository/UsersRepository.java
@@ -3,21 +3,10 @@ package com.example.VNVC_1.tiemchung.repository;
 import com.example.VNVC_1.tiemchung.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UsersRepository extends JpaRepository<Users, Long> {
-
+public interface UsersRepository extends JpaRepository<Users, Long>
+{
     Optional<Users> findByUsername(String username);
-
-    // Tìm danh sách trẻ em theo ID cha mẹ
-    List<Users> findByParentUserId(Long parentId);
-
-    // Xóa lịch booking theo ID người dùng
-    void deleteBookingByUserId(Long userId);
-
-    List<Users> findByRole(String role);
-
 }
