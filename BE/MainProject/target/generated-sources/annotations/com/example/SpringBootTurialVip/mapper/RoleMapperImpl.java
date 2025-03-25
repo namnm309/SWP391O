ed16dfbde1f93bdd1cc1c618cba66d66c0798f37
NamPC <namminhnam306@gmail.com>
2025-03-26 05:58:56 +0700

    fix conflict 2.1

--- BE/MainProject/target/generated-sources/annotations/com/example/SpringBootTurialVip/mapper/RoleMapperImpl.java
+++ BE/MainProject/target/generated-sources/annotations/com/example/SpringBootTurialVip/mapper/RoleMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-26T05:54:38+0700",
+    date = "2025-03-26T05:58:18+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 23.0.1 (Oracle Corporation)"
 )
 @Component
